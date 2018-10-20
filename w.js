72eff51d2f7cf1151d0881a4d3f9ffad71c5e386
wii3 <42401274+pannila@users.noreply.github.com>
2018-10-20 21:14:57 +0200

    Update w.js

--- w.js
+++ w.js
@@ -37,7 +37,7 @@ client.user.setGame(`,help/\,inv`)
 
 
 const sWlc = {}
-const premium = ['403640956571615243']
+const premium = ['340830120379940865']
 client.on('message', message => {
 var prefix = "-";
 if(message.channel.type === "dm") return;
@@ -81,7 +81,7 @@ client.on("guildMemberAdd", member => {
       var Canvas = require('canvas')
       var jimp = require('jimp')
       
-      const w = ['w.png', 'w1.png', 'w2.png', 'w3.png', 'w4.png', 'w5.png'];
+      const w = ['w1.png', 'w2.png', 'w3.png', 'w4.png', 'w5.png'];
       
               let Image = Canvas.Image,
                   canvas = new Canvas(557, 241),
