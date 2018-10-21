3a81bc7aa4de86fbadf5c042d8dfa721faf9402a
wii3 <42401274+pannila@users.noreply.github.com>
2018-10-21 17:22:01 +0200

    Update w.js

--- w.js
+++ w.js
@@ -123,7 +123,7 @@ client.on("guildMemberAdd", member => {
                    
                                  ctx.clip();
 
-                        ctx.drawImage(ava, 5, 8, 214, 212);
+                        ctx.drawImage(ava, 5, 8, 227, 225);
                               ctx.closePath();
 
                             
